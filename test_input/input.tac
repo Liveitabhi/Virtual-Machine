125de976e05f2394777ba514066dbe8e360cdfcd
Liveitabhi <cs17b002@iittp.ac.in>
2020-11-30 10:51:57 +0530

    VMcode to ASMcode done...

--- test_input/input.tac
+++ test_input/input.tac
@@ -1,25 +1,10 @@
-function func 0 a b
-_t2 = a + 5
-return _t2
-
-
-function func1 0 c d
-return
-
-_L4 :
-function main 3
+function main 3 int e int d
 int a
 int b
 int c
-_t1 = a == b
-If _t1 goto _L2
-pushParam a
-_t1 = b + 3
-pushParam _t1
-_t1 = call func 2
-goto _L3
-_L2 :
-_t1 = a + b
-_t2 = b + c
-_L3 :
-call func1 0
+
+_t0 = a == c
+
+_t0 = call main1 0
+
+main1 :
--- vm.h
+++ vm.h
@@ -20,6 +20,7 @@ private:
     string getVMCode(string);
     string getAssemblyCode(string);
     string getSegmentPos(Token&);
+    string memInit();
 
 public:
     VM(string, string);
@@ -61,11 +62,20 @@ void VM::vmCodeToAssembly()
     fin.open(outputTemVM);
     fout.open(ofname);
     string is, arm;
+
+    arm = memInit();
+    fout << arm;
+
     while (getline(fin, is))
     {
         arm = getAssemblyCode(is);
         fout << arm;
     }
+
+    arm = "end:\n";
+    arm += "\tj end\n";
+    fout << arm;
+
     fin.close();
     fout.close();
 }
@@ -91,10 +101,12 @@ string VM::getVMCode(string s)
     // Function/method definition and return - 3
     // Function/methods call - 4
     // Function/methods parameters - 5
-    // New Line - 6
-    
+    // Variable declaration - 6
+    // New Line - 7
+    // Syntax Error - 8
+
     int typeOfInstruction = get3acTypeOfInstruction(tokens);
-    cout<<s<<" "<<typeOfInstruction<<endl;
+
     // 3. Convert to appropriate VM Code
     string vmcode = "";
     if (typeOfInstruction == 0)
@@ -160,7 +172,7 @@ string VM::getVMCode(string s)
             vector<string> v;
             localVars.push(v);
             for(int i=3; i<tokens.size(); i++)
-                v.push_back(tokens.at(i).name);
+                v.push_back(tokens.at(++i).name);
             args.push(v);
 
             vmcode += "function " + tokens.at(1).name + " " + tokens.at(2).name + "\n";
@@ -218,8 +230,19 @@ string VM::getVMCode(string s)
     }
     else if(typeOfInstruction == 6)
     {
-        // cout << "ERROR " << endl;
+        // Variable Declaration
+        localVars.top().push_back(tokens.at(1).name);
+    }
+    else if(typeOfInstruction == 7)
+    {
+        // New line
         vmcode += "\n";
+    }
+    else
+    {
+        //Syntax Error
+        cout << "Three Address Code Syntax ERROR : -" << endl;
+        cout << s << endl;
     }
     return vmcode;
 }
@@ -228,7 +251,7 @@ int VM::get3acTypeOfInstruction(vector<Token> &v)
 {
     //New line
     if(v.empty())
-        return 6;
+        return 7;
 
     //Assignment - 0
     if (v.size() == 3 && v.at(1).name.compare("=")==0)
@@ -251,8 +274,16 @@ int VM::get3acTypeOfInstruction(vector<Token> &v)
         return 4;
 
     // Functional Parameters - 5
-    else
+    else if(v.at(0).name.compare("pushParam") == 0)
         return 5;
+
+    // Variable declaration - 6
+    else if(v.at(0).name.compare("int") == 0 || v.at(0).name.compare("char") == 0)
+        return 6;
+
+    // Syntax Error
+    else
+        return 8;
 }
 
 string VM::getSegmentPos(Token &t)
@@ -265,7 +296,7 @@ string VM::getSegmentPos(Token &t)
         return s;
     }
 
-    if(t.name.at(0)=='\'' || t.name.at(0)=='\"' || isdigit(t.name.at(0)))
+    if(t.name.at(0)=='\'' || isdigit(t.name.at(0)))
     {
         s += "constant " + t.name;
         return s;
@@ -287,12 +318,24 @@ string VM::getSegmentPos(Token &t)
         return s;
     }
 
+    cout<<"Not found!!! -> "<<t.name<<" -> adding to local"<<endl;
+
     s += "local " + to_string(localVars.top().size());
     localVars.top().push_back(t.name);
     return s;
 }
 
-string VM:: getAssemblyCode(string s)
+string VM::memInit()
+{
+    string armcode = "";
+    armcode += "li $r28,3\n";
+    armcode += "sw $r28,$zero(0)\n";
+    armcode += "sw $r28,$one(0)\n";
+    armcode += "sw $r28,$one(1)\n\n";
+    return armcode;
+}
+
+string VM::getAssemblyCode(string s)
 {
     // 1. Tokenize
     stringstream tokenizer(s);
@@ -302,10 +345,320 @@ string VM:: getAssemblyCode(string s)
         Token t(token);
         tokens.push_back(t);
     }
-    return "";
+
+
+    // 2. Match Format of VM code
+    // push - 0
+    // pop - 1
+    // Function/method declaration - 2
+    // Function/methods call - 3
+    // return - 4
+    // Arihmetic, Logical - 5
+    // if-goto - 6
+    // goto - 7
+    // Label - 8
+    // New Line - 9
+    // Syntax Error - 10
+
+    int typeOfInstruction = getVMTypeOfInstruction(tokens);
+    // cout<<s<<" -> "<<typeOfInstruction<<endl;
+    // 3. Convert to appropriate assembly Code
+    string armcode = "";
+    if (typeOfInstruction == 0)
+    {
+        // push
+
+        // temp
+        if(tokens.at(1).name.compare("temp")==0)
+        {
+            // _ti <==> r(i%26)
+            int regNo = (stoi(tokens.at(2).name))%26;
+
+            // $ri -> MEM[SP]
+            armcode += "\tlw $r28,$zero(0)\n";
+            armcode += "\tsw $r" + to_string(regNo) + ",$r28(0)\n";
+
+            // SP++
+            armcode += "\tadd $r28,$r28,$one\n";
+            armcode += "\tsw $r28,$zero(0)\n";
+        }
+
+        else
+        {
+            // local
+            if(tokens.at(1).name.compare("local")==0)
+            {
+                // $r28 <- MEM[LCL + i]
+                armcode += "\tlw $r27,$one(0)\n";
+                armcode += "\tlw $r28,$r27(" + tokens.at(2).name + ")\n";
+            }
+
+            // argument
+            else if(tokens.at(1).name.compare("argument")==0)
+            {
+                // $r28 <- MEM[ARG + i]
+                armcode += "\tlw $r27,$one(1)\n";
+                armcode += "\tlw $r28,$r27(" + tokens.at(2).name + ")\n";
+            }
+
+            // constant
+            else if(tokens.at(1).name.compare("constant")==0)
+                // $r28 <- constant
+                armcode += "\tli $r28," + tokens.at(2).name + "\n";
+
+            // $r28 -> MEM[SP]
+            armcode += "\tlw $r27,$zero(0)\n";
+            armcode += "\tsw $r28,$r27(0)\n";
+
+            // SP++
+            armcode += "\tadd $r27,$r27,$one\n";
+            armcode += "\tsw $r27,$zero(0)\n";
+
+        }
+    }
+    else if (typeOfInstruction == 1)
+    {
+        // pop
+
+        // SP--
+        armcode += "\tlw $r28,$zero(0)\n";
+        armcode += "\taddi $r28,$r28,-1\n";
+        armcode += "\tsw $r28,$zero(0)\n";
+
+        // temp
+        if(tokens.at(1).name.compare("temp")==0)
+        {
+            // _ti <==> r(i%26)
+            int regNo = (stoi(tokens.at(2).name))%26;
+
+            // $ri <- MEM[SP-1]
+            armcode += "\tlw $r" + to_string(regNo) + ",$r28(0)\n";
+        }
+
+        else
+        {
+            
+            // $r27 <- MEM[SP-1]
+            armcode += "\tlw $r27,$r28(0)\n";
+
+            // local
+            if(tokens.at(1).name.compare("local")==0)
+            {
+                // $r27 -> MEM[LCL + i]
+                armcode += "\tlw $r28,$one(0)\n";
+                armcode += "\tsw $r27,$r28(" + tokens.at(2).name + ")\n";
+            }
+
+            // argument
+            else if(tokens.at(1).name.compare("argument")==0)
+            {
+                // $r28 -> MEM[ARG + i]
+                armcode += "\tlw $r28,$one(1)\n";
+                armcode += "\tsw $r27,$r28(" + tokens.at(2).name + ")\n";
+            }
+
+        }
+    }
+    else if(typeOfInstruction == 2)
+    {
+        // Function/method declaration - 2
+        armcode += tokens.at(1).name + ":\n";
+
+        // push 0 in stack k times
+        int k = stoi(tokens.at(2).name);
+        if(k>0)
+        { 
+            armcode += "\tlw $r28,$zero(0)\n";
+            while(k--)
+            {
+                armcode += "\tsw $zero,$r28(0)\n";
+                armcode += "\tadd $r28,$r28,$one\n";
+            }
+            armcode += "\tsw $r28,$zero(0)\n";
+        }    
+    }
+    else if(typeOfInstruction == 3)
+    {
+        // Function/methods call - 3
+        
+        // push return-addr // save return-addr
+        armcode += "\tlw $r28,$zero(0)\n";
+        armcode += "\tsw $r29,$r28(0)\n";
+        armcode += "\tadd $r28,$r28,$one\n";
+
+        // push LCL // save LCL
+        armcode += "\tlw $r27,$one(0)\n";
+        armcode += "\tsw $r27,$r28(0)\n";
+        armcode += "\tadd $r28,$r28,$one\n";
+
+        // push ARG // save ARG
+        armcode += "\tlw $r27,$one(1)\n";
+        armcode += "\tsw $r27,$r28(0)\n";
+        armcode += "\tadd $r28,$r28,$one\n";
+
+        // ARG = SP - n - 3 // change ARG
+        int n = stoi(tokens.at(2).name)+3;
+        armcode += "\taddi $r27,$r28,-" + to_string(n) + "\n";
+        armcode += "\tsw $r27,$one(1)\n";
+
+        // LCL = SP // change LCL
+        armcode += "\tsw $r28,$one(0)\n";
+
+        // goto fun // call function
+        armcode += "\tjal " + tokens.at(1).name + "\n\n";
+
+        // return-addr: // a label
+        // armcode += tokens.at(1).name + "_return:\n";
+        
+    }
+    else if(typeOfInstruction == 4)
+    {
+        // return - 4
+
+        // FRAME = LCL //
+        armcode += "\tlw $r28,$one(0)\n";
+
+        // RET = *(FRAME - 3) // keep return-addr
+        armcode += "\tadd $r26,$r29,$zero\n";
+        armcode += "\taddi $r29,$r28,-3\n";
+        armcode += "\tlw $r29,$r29(0)\n";
+
+        // *ARG = pop() // return value
+        armcode += "\tlw $r27,$zero(0)\n";
+        armcode += "\taddi $r27,$27,-1\n";
+        armcode += "\tlw $r27,$r27(0)\n";
+        armcode += "\tlw $r28,$one(1)\n";
+        armcode += "\tsw $r27,$r28(0)\n";
+
+        // SP = ARG + 1 // restore SP of called
+        armcode += "\tadd $r28,$r28,$one\n";
+        armcode += "\tsw $r28,$zero(0)\n";
+
+        // ARG = *(FRAME - 1) // restore ARG
+        armcode += "\tlw $r28,$one(0)\n";
+        armcode += "\taddi $r28,$r28,-1\n";
+        armcode += "\tlw $r27,$r28(0)\n";
+        armcode += "\tsw $r27,$one(1)\n";
+
+        // LCL = *(FRAME - 2) // restore LCL
+        armcode += "\taddi $r28,$r28,-1\n";
+        armcode += "\tlw $r27,$r28(0)\n";
+        armcode += "\tsw $r27,$one(0)\n";
+
+        // goto RET // back to return-addr
+        armcode += "\tjump $r26\n";
+    }
+    else if(typeOfInstruction == 5)
+    {
+        // Arihmetic, Logical - 5
+
+        // $r28 <- SP-1
+        armcode += "\tlw $r28,$zero(0)\n";
+        armcode += "\taddi $r28,$r28,-1\n";
+
+        // $r27 <- MEM[SP-1]
+        armcode += "\tlw $r27,$r28(0)\n";
+
+        // $r28 <- SP-2
+        armcode += "\taddi $r28,$r28,-1\n";
+
+        // $r26 <- MEM[SP-2]
+        armcode += "\tlw $r26,$r28(0)\n";
+        
+        // perform operation
+        if(tokens.at(0).name.compare("add")==0)
+            armcode += "\tadd $r26,$r26,$r27\n";
+
+        else if(tokens.at(0).name.compare("sub")==0)
+            armcode += "\tsub $r26,$r26,$r27\n";
+
+        else if(tokens.at(0).name.compare("eq")==0)
+            armcode += "\tseq $r26,$r26,$r27\n";
+
+        else if(tokens.at(0).name.compare("lt")==0)
+            armcode += "\tslt $r26,$r26,$r27\n";
+
+        else if(tokens.at(0).name.compare("and")==0)
+            armcode += "\tand $r26,$r26,$r27\n";
+
+        else if(tokens.at(0).name.compare("or")==0)
+            armcode += "\tor $r26,$r26,$r27\n";
+
+        // $r26 -> MEM[SP-2]
+        armcode += "\tsw $r26,$r28(0)\n";
+
+        // $r28 <- SP-1
+        armcode += "\tadd $r28,$r28,$one\n";
+        armcode += "\tsw $r28,$zero(0)\n";
+    
+    }
+    else if(typeOfInstruction == 6)
+    {
+        // if-goto - 6
+
+        // $r27 <- MEM[SP]
+        armcode += "\tlw $r28,$zero(0)\n";
+        armcode += "\taddi $r28,$r28,-1\n";
+        armcode += "\tlw $r27,$r28(0)\n";
+
+        // branch if $r27==$one
+        armcode += "\tbeq $r27,$one," + tokens.at(1).name + "\n";
+    
+    }
+    else if(typeOfInstruction == 7)
+    {
+        // goto - 7
+        armcode += "\tjump " + tokens.at(1).name + "\n";
+
+    }
+    else if(typeOfInstruction == 8)
+    {
+        // Label
+        armcode += tokens.at(1).name + ":\n";
+    }
+    else if(typeOfInstruction == 9)
+    {
+        // New line
+        armcode += "\n";
+    }
+    else
+    {
+        //Syntax Error
+        cout << "Three Address Code Syntax ERROR : -" << endl;
+        cout << s << endl;
+    }
+    return armcode;
 }
 
 int VM::getVMTypeOfInstruction(vector<Token> &v)
 {
-    return 0;
+    //New Line
+    if(v.empty())
+        return 9;
+
+    else if(v.at(0).name.compare("push")==0)
+        return 0;
+    else if(v.at(0).name.compare("pop")==0)
+        return 1;
+    else if(v.at(0).name.compare("function")==0)
+        return 2;
+    else if(v.at(0).name.compare("call")==0)
+        return 3;
+    else if(v.at(0).name.compare("return")==0)
+        return 4;
+
+    // Arithmetic, Logical
+    else if(v.size()==1)
+        return 5;
+
+    else if(v.at(0).name.compare("if-goto")==0)
+        return 6;
+    else if(v.at(0).name.compare("goto")==0)
+        return 7;
+    else if(v.at(0).name.compare("label")==0)
+        return 8;
+
+    // Syntax Error
+    else
+        return 10;
 }
